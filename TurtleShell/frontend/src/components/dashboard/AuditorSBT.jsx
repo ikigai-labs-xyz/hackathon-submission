37204db19d6222a619f7167ac1538533daf6cbf1
keinberger <philipp.keinberger@gmail.com>
2023-06-10 18:04:57 +0200

    fix issue

--- TurtleShell/frontend/src/components/dashboard/AuditorSBT.jsx
+++ TurtleShell/frontend/src/components/dashboard/AuditorSBT.jsx
@@ -1,76 +1,75 @@
-import React from 'react';
-import Spinner from '../Spinner';
-import { BsArrowRightShort } from 'react-icons/bs';
+import React from "react"
+import Spinner from "../Spinner"
+import { BsArrowRightShort } from "react-icons/bs"
 
-import { usePrepareContractWrite, useContractWrite, useChainId, useAccount } from 'wagmi'
-import './Dashboard.css'
-import { chainIdToAdresses } from '../../utils/chainMapping';
+import { usePrepareContractWrite, useContractWrite, useChainId, useAccount } from "wagmi"
+import "./Dashboard.css"
+import { chainIdToAdresses } from "../../utils/chainMapping"
 
-const AuditorSBT = ({onSubmit}) => {
-    const chainId = useChainId()
-    const {account} = useAccount()
+const AuditorSBT = ({ onSubmit }) => {
+	const chainId = useChainId()
+	const { address: walletAddress } = useAccount()
+	const auditorNftAddress = chainIdToAdresses[chainId]?.AuditorNFT
 
-  const {
-    config,
-    error: prepareError,
-    isError: isPrepareError,
-  } = usePrepareContractWrite({
-    address: chainIdToAdresses[chainId].AuditorNFT,
-    abi: [
-      {
-        name: 'mint',
-        type: 'function',
-        stateMutability: 'nonpayable',
-        inputs: [
-            {
-              internalType: "address",
-              name: "auditor",
-              type: "address"
-            }
-          ],
-        outputs: [],
-      },
-    ],
-    functionName: "mint",
-    args: [account],
+	const {
+		config,
+		error: prepareError,
+		isError: isPrepareError,
+	} = usePrepareContractWrite({
+		address: auditorNftAddress,
+		abi: [
+			{
+				name: "mint",
+				type: "function",
+				stateMutability: "nonpayable",
+				inputs: [
+					{
+						internalType: "address",
+						name: "auditor",
+						type: "address",
+					},
+				],
+				outputs: [],
+			},
+		],
+		functionName: "mint",
+		args: [walletAddress],
+	})
 
-  })
+	const { write, isSuccess } = useContractWrite(config)
 
+	const handleButtonClick = async () => {
+		console.log(chainIdToAdresses[chainId].AuditorNFT)
+		write?.()
+		console.log(prepareError, write, isSuccess)
 
+		if (isSuccess) {
+			Promise.resolve(onSubmit("mint badge"))
+		}
+	}
 
-  const { write, isSuccess } = useContractWrite(config)
+	return (
+		<div>
+			<h1 className="h1">Get Started</h1>
+			<h2 className="h2">
+				First of all, you will need to get verified as an Auditor on TurtleShell. Therefore, you will
+				need an Auditor SBT.
+			</h2>
 
-  const handleButtonClick = async () => {
-    console.log(chainIdToAdresses[chainId].AuditorNFT)
-    write?.()
-    console.log(prepareError, write, isSuccess)
+			<div className="text-center">
+				<button
+					className={`px-4 py-2 mb-3 text-sm font-semibold bg-gradient-to-br from-[#5C2C69] to-#2C4C84 border-transparent rounded-full w-fit text-[#C2C2C2]`}
+					onClick={() => {
+						handleButtonClick()
+					}}
+				>
+					<div className="flex items-center">
+						Mint Badge <BsArrowRightShort size={20} />
+					</div>
+				</button>
+			</div>
+		</div>
+	)
+}
 
-    if (isSuccess) {
-      Promise.resolve(onSubmit('mint badge'))
-    }
-  };
-
-  return (
-    <div>
-      <h1 className='h1'>Get Started</h1>
-      <h2 className='h2'>
-        First of all, you will need to get verified as an Auditor on TurtleShell. Therefore, you will need an Auditor SBT.
-      </h2>
-
-      <div className="text-center">
-        <button
-          className={`px-4 py-2 mb-3 text-sm font-semibold bg-gradient-to-br from-[#5C2C69] to-#2C4C84 border-transparent rounded-full w-fit text-[#C2C2C2]`}
-          onClick={() => {handleButtonClick(); }}
-        >
-          <div className="flex items-center">
-            Mint Badge <BsArrowRightShort size={20} />
-          </div>
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default AuditorSBT;
-
-
+export default AuditorSBT
--- TurtleShell/frontend/src/utils/chainMapping.js
+++ TurtleShell/frontend/src/utils/chainMapping.js
@@ -1,44 +1,56 @@
 const chainIdToName = {
-  1: "mainnet",
-  5: "goerli",
-  137: "polygon",
-  80001: "mumbai",
-  43114: "avalanche",
-  420: "optimism",
-  1422: "zkevm",
-  3141: "filecoin",
-  100: "gnosis",
-
+	1: "mainnet",
+	5: "goerli",
+	137: "polygon",
+	80001: "mumbai",
+	43114: "avalanche",
+	420: "optimism",
+	1422: "zkevm",
+	3141: "filecoin",
+	100: "gnosis",
 }
 
 const chainIdToExplorerUrl = {
-  1: "https://etherscan.io",
-  5: "https://goerli.etherscan.io",
-  137: "https://polygonscan.com",
-  80001: "https://mumbai.polygonscan.com",
-  43114: "https://snowtrace.io",
-  420: "https://goerli-optimism.etherscan.io",
-  1422: "https://explorer.public.zkevm-test.net",
-  3141: "https://hyperspace.filfox.info/en",
-  100: "https://rpc.gateway.pokt.network/",
+	1: "https://etherscan.io",
+	5: "https://goerli.etherscan.io",
+	137: "https://polygonscan.com",
+	80001: "https://mumbai.polygonscan.com",
+	43114: "https://snowtrace.io",
+	420: "https://goerli-optimism.etherscan.io",
+	1422: "https://explorer.public.zkevm-test.net",
+	3141: "https://hyperspace.filfox.info/en",
+	100: "https://rpc.gateway.pokt.network/",
 }
 
 const chainIdToAdresses = {
+	2357: {
+		AuditorNFT: "0x5D7e8eBfa2716B4752f609A16933b464a4ffa449",
+		SmartContractNFT: "0xA3B1Ed01730fbeFB4ae0b33456Ae59C8192ac5CB",
+	},
+	11155111: {
+		AuditorNFT: "0x5D7e8eBfa2716B4752f609A16933b464a4ffa449",
+		SmartContractNFT: "0xA3B1Ed01730fbeFB4ae0b33456Ae59C8192ac5CB",
+	},
 
-  11155111: {AuditorNFT:'0x5D7e8eBfa2716B4752f609A16933b464a4ffa449',
-             SmartContractNFT:'0xA3B1Ed01730fbeFB4ae0b33456Ae59C8192ac5CB'},
+	10200: {
+		AuditorNFT: "0xC4306e51954c8a266299b68d95915797582E833F",
+		SmartContractNFT: "0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D",
+	},
 
-  10200: {AuditorNFT:'0xC4306e51954c8a266299b68d95915797582E833F',
-            SmartContractNFT:'0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D'},
-  
-167005: {AuditorNFT:'0xC4306e51954c8a266299b68d95915797582E833F',
-        SmartContractNFT:'0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D'},
+	167005: {
+		AuditorNFT: "0xC4306e51954c8a266299b68d95915797582E833F",
+		SmartContractNFT: "0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D",
+	},
 
-        420: {AuditorNFT:'0xC4306e51954c8a266299b68d95915797582E833F',
-        SmartContractNFT:'0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D'},
+	420: {
+		AuditorNFT: "0xC4306e51954c8a266299b68d95915797582E833F",
+		SmartContractNFT: "0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D",
+	},
 
-        535453: {AuditorNFT:'0xC4306e51954c8a266299b68d95915797582E833F',
-        SmartContractNFT:'0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D'},
+	535453: {
+		AuditorNFT: "0xC4306e51954c8a266299b68d95915797582E833F",
+		SmartContractNFT: "0x2A7f14c77Fd1f8E4B1a68F833f8D614a86f5538D",
+	},
 }
 
 export { chainIdToName, chainIdToExplorerUrl, chainIdToAdresses }
