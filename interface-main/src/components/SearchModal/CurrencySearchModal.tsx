4a2ead0e5b97cf63fd09cfcee139f01ecb587f07
Tobias Leinß <tobias.leinss@mway.io>
2023-06-10 17:10:57 +0200

    Fix import

--- interface-main/src/components/SearchModal/CurrencySearchModal.tsx
+++ interface-main/src/components/SearchModal/CurrencySearchModal.tsx
@@ -10,7 +10,7 @@ import Modal from "../Modal"
 import { CurrencySearch } from "./CurrencySearch"
 import SafetyWarning from "pages/AddLiquidity/SafetyWarning"
 
-import smartContractArtifact from "pages/AddLiquidity/SmartContractNFT.json"
+import smartContractArtifact from "pages/AddLiquidity/SmartContractNft.json"
 import securedDexArtifact from "pages/AddLiquidity/SecuredDex.json"
 import { ethers } from "ethers"
 import { useWeb3React } from "@web3-react/core"
@@ -79,7 +79,9 @@ export default memo(function CurrencySearchModal({
           !urls[chainId?.toString()] ||
           !urls[chainId?.toString()].SmartContractNFT
         ) {
-          throw new Error(`No smart contract deployment found for chainId ${chainId}`)
+          throw new Error(
+            `No smart contract deployment found for chainId ${chainId}`
+          )
         }
 
         const smartContractNft = new ethers.Contract(
